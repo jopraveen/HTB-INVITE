20296fd8b823c406c2c0c34bb82d95cfba271c5a
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-01-02 23:31:18 +0530

    Solved many errors

--- android.requirements.sh
+++ android.requirements.sh
@@ -1,4 +1,8 @@
 apt install figlet
+apt install python
+pip install termcolor
+apt install ruby
 git clone https://github.com/busyloop/lolcat
 cd lolcat/bin && gem install lolcat
 clear
+echo "REQUIREMENTS SATISFIED" | lolcat -a -d 5
